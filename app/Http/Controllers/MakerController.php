cd6f77d16460e8195a3227feff338aa34a5d8118
anderson.silva <andersonsrsilva@gmail.com>
2016-10-28 09:58:54 -0200

    Controllers functions

--- app/Http/Controllers/MakerController.php
+++ app/Http/Controllers/MakerController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Maker;
 use Illuminate\Http\Request;
 
 class MakerController extends Controller
 {
-    //
+    public function index()
+    {
+        $makers = Maker::all();
+
+        return response()->json($makers, 200);
+    }
+
+    public function show($id)
+    {
+        $makers = Maker::find($id);
+
+        if (!$makers) {
+            return response()->json(['message' => 'This maker does not exist', 'code' => 404], 404);
+        }
+
+        return response()->json($makers, 200);
+    }
 }
--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Vehicle;
 use Illuminate\Http\Request;
 
 class VehicleController extends Controller
 {
-    //
+    public function index()
+    {
+        $vehicles = Vehicle::all();
+
+        return response()->json($vehicles, 200);
+    }
 }
