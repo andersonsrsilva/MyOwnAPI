3dbadc9d5dea5a269b93c0004722a73430eb4ac3
anderson.silva <andersonsrsilva@gmail.com>
2016-11-07 09:57:47 -0200

    File

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\FileRequest;
+use Carbon\Carbon;
 
 class FileController extends Controller
 {
+    public function index()
+    {
+        return "index";
+    }
+
+    public function store(FileRequest $request)
+    {
+        return "store";
+
+        $file = $request->file('file');
+        $title = $request->get('title');
+        $description = $request->get('description');
+        $path = '/files/';
+        $name = sha1(Carbon::now()) . '.' . $file->guessExtension();
+
+        $file->move(public_path() . $path, $name);
+
+        $instance = File::create([
+            'title' => $title,
+            'description' => $description,
+            'path' => $path.$name
+        ]);
+
+        return response()->json(['data' => "The file {$instance->name} was created with id {$instance->id}"], 200);
+    }
+
     public function show($id)
     {
     }
